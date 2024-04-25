e6563988cc18c4eee25a279318163905c3a6d7bc
Sven <147499202+SvenZi@users.noreply.github.com>
2024-04-25 16:15:13 +0200

    2_Loginseite
    Loginseite erster Entwurf

--- Web-Dev_Notizwebseite/start/routes.ts
+++ Web-Dev_Notizwebseite/start/routes.ts
@@ -7,9 +7,17 @@
 |
 */
 
+import { Redirect } from '@adonisjs/core/http';
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
 
 
 router.on('/login').render('pages/login')
+
+router.post('/login', async ({ request }) => {
+    const name = request.input('name');
+    const password = request.input('password');
+
+    return name
+});
